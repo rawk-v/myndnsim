3fdf22fddadc41abdfe3f8a6f3864dcd377f0746
wuhuanyu <1790949749@qq.ccom>
2016-06-03 21:11:51 +0800

    installscript

---
+++ src/install.sh
@@ -0,0 +1,30 @@
+sudo apt-get install gcc g++ python -y
+sudo  apt-get install gcc g++ python python-dev -y
+sudo apt-get install mercurial -y
+sudo apt-get instal bzr -y
+sudo apt-get install gdb valgrind -y
+
+sudo apt-get install gsl-bin libgsl0-dev libgsl0ldbl -y
+
+sudo apt-get install flex bison libfl-dev -y
+sudo apt-get install tcpdump -y
+
+sudo apt-get install sqlite sqlite3 libsqlite3-dev -y
+
+sudo apt-get install libxml2 libxml2-dev -y
+
+sudo apt-get install libgtk2.0-0 libgtk2.0-dev -y
+sudo apt-get install vtun lxc -y
+sudo apt-get install uncrustify -y
+sudo apt-get install doxygen graphviz imagemagick -y
+
+sudo apt-get install  texlive texlive-extra-utils texlive-latex-extra -y
+sudo apt-get install  python-sphinx dia -y
+sudo apt-get install  python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev -y
+
+
+sudo apt-get install  libboost-signals-dev libboost-filesystem-dev -y
+sudo apt-get install  openmpi* -y
+sudo apt-get install build-esscential -y
+sudo apt-get install libboost-all-dev -y
+
